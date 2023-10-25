4f35b65440517c9b6f8303a9378e5fb1cd2ad0d8
Akir Oussama <akir.oussama@gmail.com>
2023-10-25 10:16:01 +0200

    test pr merge

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          this is a new branch !
         </p>
       </header>
     </div>
